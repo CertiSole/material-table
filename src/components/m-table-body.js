b0ad10477eada19e1b08c0761b00ba49237a5006
Matthieu Pilleul <matthieu.pilleul@epitech.eu>
2023-08-10 13:29:21 +0200

    Update table body component

--- src/components/m-table-body.js
+++ src/components/m-table-body.js
@@ -56,8 +56,17 @@ class MTableBody extends React.Component {
             )}
             key="empty-"
           >
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABjklEQVR4nO2TuS+EURTFfxIJirFFgwqFrSFaItNSiGh0DLElEmYsQVQ0ev+FLRqVJTpLTQhii4YCM9EQMvKSY/Ly5c0Yo5liTvIl3z33nnPfu+89yCDdUA6E/qEPycOJPOAYiAJ9KZj3SWs8cl0F8yq4BkpSaFAEXMhj1pssBl6VbBFnVlH1xyat8nhRwxj6ldhWXA88iDsBJoBmwA9MAVvAUpwmu9IFbHJD5IDiTcX7QFj/rs+s2Ish5VZt8md21Yq7gC/gBmgAOrXyMaANaAQ+gHOgwtOgVl5nNhkR6bO4EeATeAPGdctsTEvzDqwDPeLzxYd/a4BGcK/cI7AMdOjwzSVot652VOYF+n91jajGMVMfMAnceuZvNAbZwJ3GZVDnGtGayEHiIwtoAkY1lmegFFiUdkF1w4pXXK9wJ0EDe0cHqo/onE6tM9pTrjem0KN4SXD1XLvp1tierNH65WF2V+gVzSl5qZedDHKBSv0bzZU8ZuIVH7leYZIISHsI5MQrKgOCpI6gPDJII3wDfjRztnYGUBAAAAAASUVORK5CYII=" />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="https://img.icons8.com/?size=192&amp;id=BKevtZc6mL9s&amp;format=png"
+                style={{ width: "26px", height: "26px" }}
+              />
               {localization.emptyDataSourceMessage}
             </div>
           </TableCell>
